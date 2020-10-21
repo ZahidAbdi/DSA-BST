d6a5600cad83f6670cd993befdd5136ad78bee76
Zablackafro <zahidabdikadir12@gmail.com>
2020-10-20 20:16:51 -0500

    done

---
+++ logic.js
@@ -0,0 +1,36 @@
+// 1. Draw a BST
+//       (4)
+//   (3)    (5)
+// (1) (2)     (6)
+//           (7)  (9)
+
+//     (3)
+// (1)     (4)
+//    (2)     (6)
+//         (5)     (9)
+//              (7)
+// E A S Y Q U E S T I O N
+// Assumptions: Duplicates go Right on the Tree, Duplicates are allowed
+//         (E)
+//   (A)                (S)
+//               (Q)             (Y)
+//           (E)             (U)
+//              (I)       (S)
+//                 (O)       (T)
+//              (N)
+
+// 2. Remove the root
+
+// 3. Create a BST class
+
+// 4. What does this program do?
+
+// 5. Height of a BST
+
+// 6. Is it a BST?
+
+// 7. 3rd largest node
+
+// 8. Balanced BST
+
+// 9. Are they the same BSTs?
